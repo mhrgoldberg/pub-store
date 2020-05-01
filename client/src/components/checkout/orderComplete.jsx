b569e1ebd1de147b85faeae76c2c74b5c1ed57c7
mitchellreiss <mitchellreiss@gmail.com>
2020-05-01 16:47:14 -0700

    Correct orderComplete formatting

--- client/src/components/checkout/orderComplete.jsx
+++ client/src/components/checkout/orderComplete.jsx
@@ -3,38 +3,39 @@ import { withRouter } from "react-router-dom";
 import axios from "axios";
 
 const OrderComplete = ({ match }) => {
-	const [data, setData] = useState({});
+  const [data, setData] = useState({});
   useEffect(() => {
     axios
       .get(`/api/orders/${match.params.orderId}`)
       .then((payload) => {
-				setData(payload.data)
-				console.log(payload.data)
-			})
+        setData(payload.data);
+        console.log(payload.data);
+      })
       .catch((error) => console.log(error));
-	}, []);
-	let orderId, address, city, state, zip;
-	let products = [];
-	
-	if (data.order) {
-		orderId = data.order.id;
-		address = data.order.address;
-		city = data.order.city;
-		state = data.order.state;
-		zip = data.order.zip;
-		products = data.products;
-	}
-  return <div>
-		<span>{orderId}</span>
-		<span>{address}</span>
-		<span>{city}</span>
-		<ul>
-			{products.map( product => (
-			<li>{product.title}</li>
-			))}
+  }, []);
+  let orderId, address, city, state, zip;
+  let products = [];
 
-		</ul>
-	</div>;
+  if (data.order) {
+    orderId = data.order.id;
+    address = data.order.address;
+    city = data.order.city;
+    state = data.order.state;
+    zip = data.order.zip;
+    products = data.products;
+  }
+  return (
+    <div>
+      <span>{orderId}</span>
+      <span>{address}</span>
+      <span>{city}</span>
+      <ul>
+        {products.map((product) => (
+          <li>{product.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default withRouter(OrderComplete);
