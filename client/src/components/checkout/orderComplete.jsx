8aa15f91522b823ea19539b7c477038fffab4ac2
mitchellreiss <mitchellreiss@gmail.com>
2020-05-01 16:46:45 -0700

    Update orderComplete to wait for DB payload

--- client/src/components/checkout/orderComplete.jsx
+++ client/src/components/checkout/orderComplete.jsx
@@ -9,24 +9,29 @@ const OrderComplete = ({ match }) => {
       .get(`/api/orders/${match.params.orderId}`)
       .then((payload) => {
 				setData(payload.data)
+				console.log(payload.data)
 			})
       .catch((error) => console.log(error));
 	}, []);
-	const orderId;
-	const address;
-	const products;
+	let orderId, address, city, state, zip;
+	let products = [];
+	
 	if (data.order) {
 		orderId = data.order.id;
 		address = data.order.address;
+		city = data.order.city;
+		state = data.order.state;
+		zip = data.order.zip;
 		products = data.products;
 	}
   return <div>
 		<span>{orderId}</span>
-		<span>{data.order ? data.order.address: null}</span>
+		<span>{address}</span>
+		<span>{city}</span>
 		<ul>
-			{products.map( product => {
-			<li>product</li>
-			})}
+			{products.map( product => (
+			<li>{product.title}</li>
+			))}
 
 		</ul>
 	</div>;
