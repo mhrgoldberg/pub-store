f625d5ca800154d8a825fed42bf4251b20c671db
mitchellreiss <mitchellreiss@gmail.com>
2020-05-01 21:27:13 -0700

    Add feature update cart qty on "enter"

--- client/src/components/cart/cartItem.jsx
+++ client/src/components/cart/cartItem.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   InputGroup,
   FormControl,
@@ -6,7 +6,7 @@ import {
   Button,
   ButtonGroup,
   Row,
-  Col
+  Col,
 } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
@@ -15,6 +15,11 @@ const CartItem = ({ data, cartQuantity, setCart, cart, setShowErrorToast }) => {
   const [localCartQuantity, setLocalCartQuantity] = useState(cartQuantity);
   const [editQty, setEditQty] = useState(false);
   let qtyComponent;
+
+  useEffect(() => {
+    updateCartItem();
+  }, [localCartQuantity]);
+
   const updateCartItem = () => {
     if (Number(localCartQuantity) - data.quantity <= stockQuantity) {
       setStockQuantity(stockQuantity - localCartQuantity);
@@ -27,21 +32,28 @@ const CartItem = ({ data, cartQuantity, setCart, cart, setShowErrorToast }) => {
         });
       });
     } else {
-      setShowErrorToast(true)
+      setShowErrorToast(true);
       setLocalCartQuantity(cart[data.id].cartQuantity);
-      
     }
   };
 
+  const keyPressed = (e) => {
+    if (e.keyCode === 13) {
+      e.persist();
+      setLocalCartQuantity(e.target.value);
+      setEditQty(!editQty);
+    }
+  };
 
   if (editQty) {
     qtyComponent = (
       <InputGroup>
         <InputGroup.Prepend>
-          <InputGroup.Text id="btnGroupAddon">Qty</InputGroup.Text>
+          <InputGroup.Text id="btnGroupAddon"> Qty</InputGroup.Text>
         </InputGroup.Prepend>
         <FormControl
           value={localCartQuantity}
+          onKeyDown={keyPressed}
           onChange={(e) => {
             e.persist();
             setLocalCartQuantity(e.target.value);
@@ -56,44 +68,41 @@ const CartItem = ({ data, cartQuantity, setCart, cart, setShowErrorToast }) => {
     qtyComponent = <Col>Qty: {localCartQuantity}</Col>;
   }
 
-  
   return (
     <ListGroup.Item className="cart-li">
-      <Row style={{width: "100%"}}>
-      <Col className="cart-control">{data.title}</Col>
-      <Col className="cart-control">
-        {qtyComponent}
-      </Col>
-      <Col lg="2" className="cart-control">
-        <ButtonGroup>
-        <Button
-          variant={!editQty ? "warning" : "success"}
-          onClick={() => {
-            if (editQty) {
-              updateCartItem();
-              setEditQty(!editQty);
-            } else {
-              setEditQty(!editQty);
-            }
-          }}
-        >
-          <FontAwesomeIcon icon="edit" style={{ color: "white" }} />
-        </Button>
-        <Button
-          variant="danger"
-          onClick={() => {
-            setStockQuantity(stockQuantity + cartQuantity)
-            setCart((prevState) => {
-              let newState = Object.assign({}, prevState);
-              delete newState[data.id];
-              return newState;
-            });
-          }}
-        >
-          <FontAwesomeIcon icon="trash" />
-        </Button>
-        </ButtonGroup>
-      </Col>
+      <Row style={{ width: "100%" }}>
+        <Col className="cart-control">{data.title}</Col>
+        <Col className="cart-control">{qtyComponent}</Col>
+        <Col lg="2" className="cart-control">
+          <ButtonGroup>
+            <Button
+              variant={!editQty ? "warning" : "success"}
+              onClick={() => {
+                if (editQty) {
+                  updateCartItem();
+                  setEditQty(!editQty);
+                } else {
+                  setEditQty(!editQty);
+                }
+              }}
+            >
+              <FontAwesomeIcon icon="edit" style={{ color: "white" }} />
+            </Button>
+            <Button
+              variant="danger"
+              onClick={() => {
+                setStockQuantity(stockQuantity + cartQuantity);
+                setCart((prevState) => {
+                  let newState = Object.assign({}, prevState);
+                  delete newState[data.id];
+                  return newState;
+                });
+              }}
+            >
+              <FontAwesomeIcon icon="trash" />
+            </Button>
+          </ButtonGroup>
+        </Col>
       </Row>
     </ListGroup.Item>
   );
--- client/src/components/checkout/orderComplete.jsx
+++ client/src/components/checkout/orderComplete.jsx
@@ -1,5 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { withRouter } from "react-router-dom";
+import {
+  Container,
+  Row,
+  Col,
+  ListGroup,
+  Alert,
+  ProgressBar,
+  Button,
+} from "react-bootstrap";
 import axios from "axios";
 
 const OrderComplete = ({ match }) => {
@@ -13,27 +22,38 @@ const OrderComplete = ({ match }) => {
       })
       .catch((error) => console.log(error));
   }, []);
-  let orderId, address, city, state, zip;
+	let orderId = "";
+	let address = "";
+	let city = "";
+	let state = "";
+	let zip = "";
   let products = [];
 
   if (data.order) {
     orderId = data.order.id;
     address = data.order.address;
-    city = data.order.city;
+    city = `${data.order.city},`;
     state = data.order.state;
     zip = data.order.zip;
     products = data.products;
   }
   return (
-    <div>
-      <span>{orderId}</span>
-      <span>{address}</span>
-      <span>{city}</span>
-      <ul>
-        {products.map((product) => (
-          <li>{product.title}</li>
-        ))}
-      </ul>
+    <div className="main-container">
+      <Container className="final-cart">
+        <br />
+        <ProgressBar animated now={100} />
+        <br />
+        <Row>{orderId}</Row>
+				<br />
+        <Row>{address}</Row>
+        <Row>{city} {state} {zip}</Row>
+				<Row>Your order of: </Row>
+        <ListGroup>
+          {products.map((product) => (
+            <ListGroup.Item>{product.title}</ListGroup.Item>
+          ))}
+        </ListGroup>
+      </Container>
     </div>
   );
 };
